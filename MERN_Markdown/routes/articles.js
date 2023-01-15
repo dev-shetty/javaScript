471c3dbf6dcaa6959c59e5ce912750e6ed21c204
Deveesh Shetty <deveeshshetty@gmail.com>
2023-01-15 16:51:46 +0530

    Half way through...was not the tutorial i expected

--- MERN_Markdown/routes/articles.js
+++ MERN_Markdown/routes/articles.js
@@ -1,9 +1,28 @@
 const express = require("express")
-
+const Article = require("../models/article")
 const router = express.Router()
 
-router.get("/", (req, res) => {
-  res.send("In articles")
+router.get("/new", (req, res) => {
+  res.render("articles/new", { article: new Article() })
+})
+
+router.get("/:id", (req, res) => {
+  res.send(req.params.id)
+})
+
+router.post("/", async (req, res) => {
+  let article = new Article({
+    title: req.body.title,
+    description: req.body.description,
+    markdown: req.body.markdown,
+  })
+  try {
+    article = await article.save()
+    res.redirect(`/articles/${article.id}`)
+  } catch (error) {
+    console.log(error)
+    res.render("articles/new", { article: article })
+  }
 })
 
 module.exports = router
--- MERN_Markdown/server.js
+++ MERN_Markdown/server.js
@@ -1,14 +1,34 @@
 const express = require("express")
+const mongoose = require("mongoose")
 const app = express()
 const articleRouter = require("./routes/articles")
 
+mongoose.set("strictQuery", true)
+mongoose.connect("mongodb://localhost/articleBlog", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+
 app.set("view engine", "ejs")
 
-app.use("/articles", articleRouter)
+app.use(express.urlencoded({ extended: false }))
 
 app.get("/", (req, res) => {
-  res.render("index")
+  const articles = [
+    {
+      title: "Test Article",
+      createdAt: new Date(),
+      description: "test description",
+    },
+    {
+      title: "Test Article",
+      createdAt: new Date(),
+      description: "test description",
+    },
+  ]
+  res.render("articles/index", { articles: articles })
 })
+app.use("/articles", articleRouter)
 
 app.listen(5000, () => {
   console.log("Server started")
