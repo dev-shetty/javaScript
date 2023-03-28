e16ffc98d99a84dfbe9c2b1103be92f93c60304b
Deveesh Shetty <deveeshshetty@gmail.com>
2023-03-28 18:28:40 +0530

    ADD: Static content

--- FCCThreeJS/src/App.jsx
+++ FCCThreeJS/src/App.jsx
@@ -1,11 +1,17 @@
+import DispaySection from "./components/DisplaySection"
+import Jumbotron from "./components/Jumbotron"
+import Nav from "./components/Nav"
+import SoundSection from "./components/SoundSection"
 
 function App() {
-
   return (
     <div className="App">
-      Hello
+      <Nav />
+      <Jumbotron />
+      <SoundSection />
+      <DispaySection /> 
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
